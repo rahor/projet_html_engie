27f716e334bcdbff14e8e4435b4eee45b2c1303c
Lucien <lucien.crn@gmail.com>
2018-11-28 14:45:24 +0100

    v1

--- index.php
+++ index.php
@@ -20,15 +20,60 @@
 	<!--MAIN-->
 	<main>
 
-	<h1>La page d'accueil</h1>
-	<h2>Le monserrat</h2>
-	<h3>Le nunito</h3>
-	
+		<!--TEST DE POLICE-->
+		<h1>La page d'accueil</h1>
+		<h2>Le monserrat</h2>
+		<h3>Le nunito marche c'est cool</h3>
+
+		<section class="sectionActu">
+			<h1>DERNI&Egrave;RES ACTUALIT&Egrave;S - carnet de route</h1>
+		</section>
+
+		<section class="sectionEquipe">
+			<div class="container">
+			<h1>D&Eacute;COUVREZ L'&Eacute;QUIPE 2016</h1>
+			
+			</div>
+		</section>
+		
+		<section class="sectionHC">
+			<h1>HORS-COURSE</h1>
+		</section>
+		
+		<section class="sectionProchainesCourses">
+			<article>
+				<h1>PROCHAINES COURSE</h1>
+				<ul>
+					<li>Titre de la course</li>
+					<li>Titre de la course</li>
+					<li>Titre de la course</li>
+					<li>Titre de la course</li>
+				</ul>
+				<a href="#">toutes les courses</a>
+			</article>
+			
+			<article>
+				<h1>ZOOM SUR LE TOUR D'ESPAGNE</h1>
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nam dignissimos quae excepturi, architecto fuga repudiandae. Fuga sed quod quibusdam, odio, enim debitis blanditiis id eaque in obcaecati optio aut ea.</p>
+				
+				<a href="https://placeholder.com"><img src="https://via.placeholder.com/150"></a>
+
+				<a href="#">découvrir les courses</a>
+			</article>
+		
+		</section>
+
+
 	</main>
 
 	<!--FOOTER-->
 	<?php
 	include('./pages/include/footer.html');
 	?>
+
 </body>
 </html>
+
+
+
+
