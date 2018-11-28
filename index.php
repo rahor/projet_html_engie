918b2d3eb25436bdb15835807290c0f3e00e0366
Rhor <hor.raphaell@gmail.com>
2018-11-28 12:01:34 +0100

    premier commit

---
+++ index.php
@@ -0,0 +1,23 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8" />
+	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+	<link rel="stylesheet" type="text/css" href="./css/style.css" />
+	<title>Energie_Team</title>
+</head>
+<body>
+
+<?php
+include('./pages/include/header.html');
+?>
+
+<h1>La page html</h1>
+
+<?php
+include('./pages/include/footer.html');
+?>
+
+</body>
+</html>
