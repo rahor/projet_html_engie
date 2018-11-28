856231d6536e128c2e908df84e07a0303c30d16a
Rhor <hor.raphaell@gmail.com>
2018-11-28 12:27:27 +0100

    polices

--- index.php
+++ index.php
@@ -4,6 +4,11 @@
 <head>
 	<meta charset="utf-8" />
 	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+	<!-- <link href="https://fonts.googleapis.com/css?family=Courgette|Open+Sans" rel="stylesheet">
+	montserat
+	nunito -->
+	<link href="https://fonts.googleapis.com/css?family=Montserrat|Nunito" rel="stylesheet">
+
 	<link rel="stylesheet" type="text/css" href="./css/style.css" />
 	<title>Energie_Team</title>
 </head>
@@ -16,6 +21,8 @@
 	<main>
 
 	<h1>La page d'accueil</h1>
+	<h2>Le monserrat</h2>
+	<h3>Le nunito</h3>
 	
 	</main>
 
