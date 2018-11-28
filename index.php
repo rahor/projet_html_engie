6620c630ecccd1f4b1a9090b4b28b4807ce00653
Rhor <hor.raphaell@gmail.com>
2018-11-28 13:35:48 +0100

    pushTest13

--- index.php
+++ index.php
@@ -4,6 +4,11 @@
 <head>
 	<meta charset="utf-8" />
 	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+	<!-- <link href="https://fonts.googleapis.com/css?family=Courgette|Open+Sans" rel="stylesheet">
+	montserat
+	nunito -->
+	<link href="https://fonts.googleapis.com/css?family=Montserrat|Nunito" rel="stylesheet">
+
 	<link rel="stylesheet" type="text/css" href="./css/style.css" />
 	<title>Energie_Team</title>
 </head>
@@ -15,23 +20,22 @@
 	<!--MAIN-->
 	<main>
 
-	<h1>La page d'accueil</h1>
-	
+		<!--TEST DE POLICE-->
+		<h1>La page d'accueil</h1>
+		<h2>Le monserrat</h2>
+		<h3>Le nunito marche c'est cools</h3>
+
+		<section class="sectionActu"></section>
+		<section class="sectionEquipe"></section>
+		<section class="sectionHC"></section>
+		<section class="sectionProchainesCourses"></section>
+
 	</main>
 
 	<!--FOOTER-->
 	<?php
 	include('./pages/include/footer.html');
 	?>
-<?php
-include('./header.html');
-?>
 
-<h1>La page html</h1>
-ada,ma
-
-<?php
-include('./header.html');
-?>
 </body>
 </html>
