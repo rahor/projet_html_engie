e5c58e6eb887527324b1c9f962667072f02ba2a3
Rhor <hor.raphaell@gmail.com>
2018-11-28 13:07:34 +0100

    ajouts d'image

---
+++ index.php
@@ -0,0 +1,24 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8" />
+	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+	<link rel="stylesheet" type="text/css" href="./css/style.css" />
+	<title>Energie_Team</title>
+</head>
+<body>
+
+<?php
+include('./header.html');
+?>
+
+<h1>La page html</h1>
+ada,ma
+
+<?php
+include('./header.html');
+?>
+
+</body>
+</html>
