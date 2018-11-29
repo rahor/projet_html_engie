d22e3ee0ab0f830ece2798f94cc9f8634b4fa0fa
Lucien <lucien.crn@gmail.com>
2018-11-29 10:48:49 +0100

    clean img

---
+++ pages/horsCourse.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Hors Course</title>
+    <link rel="stylesheet" type="text/css" href="../css/horsecourse.css">
+  </head>
+  <body>
+
+    <!--Header-->
+    <?php  include('./include/header.html'); ?>
+
+
+
+    <!--Footer-->
+    <?php include('./include/footer.html')?>
+
+  </body>
+</html>
