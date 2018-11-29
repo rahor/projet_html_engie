0af446f0942748480814fe8ed950adf9018160b7
Lucien <lucien.crn@gmail.com>
2018-11-29 13:40:46 +0100

    page hors course html

--- pages/pagehorscourse.php
+++ pages/pagehorscourse.php
@@ -10,9 +10,126 @@
     <!--Header-->
     <?php  include('./include/header.html'); ?>
 
-    <!--Fil d'ariane-->
+    <main>
+
+      <!--Fil d'ariane + Titre + icone + image-->
+
+                            <!--       Background color -> #f0f6ff      -->
+      <section class="">
+
+        <!--Fil d'ariane-->
+        <div>
+          <!--Faire un cercle en css autour de la fleche gauche-->
+          <img src="../img/icone/flecheg.png">
+          <h2>HORS-COURSE</h2>
+          <img src="../img/icone/fleched.png">
+          <h2>INDISCRETIONS</h2>
+        </div>
+
+        <!--Titre + icone + image-->
+        <div>
+
+          <!--Titre-->
+          <p>Ex turba vero imae sortis et paupertinae in tabernis</p>
+
+          <!--Icone
+          Faire css titre icone
+          -->
+          <div>
+            <img src="../img/icone/iconmic.png" class="">
+            <h2 class="">INDISCRETIONS</h2>
+          </div>
+
+          <!--1er image-->
+          <img src="../img/pagehorscourse/img1.png" class="">
+
+        </div>
+
+      </section>
+
+      <!--text + photo-->
+      <section>
+
+        <!--Para-->
+        <!--1er para en BOLD-->
+        <p class="">
+           Ex turba vero imae sortis et paupertinae in tabernis aliqui pernoctant vinariis, non nulli velariis umbraculorum theatralium latent,
+           quae Campanam imitatus lasciviam Catulus in aedilitate sua suspendit omnium primus;
+        </p>
+
+        <!--Para normal-->
+        <p>
+           aut pugnaciter aleis certant turpi sono fragosis naribus introrsum reducto spiritu concrepantes;
+           aut quod est studiorum omnium maximum ab ortu lucis ad vesperam sole fatiscunt vel pluviis,
+           per minutias aurigarum equorumque praecipua vel delicta scrutantes.aut pugnaciter aleis certant turpi
+           sono fragosis naribus introrsum reducto spiritu concrepantes;
+           aut quod est studiorum omnium maximum ab ortu lucis ad vesperam sole fatiscunt vel pluviis
+        </p>
+
+        <p>
+          aut quod est studiorum omnium maximum ab ortu lucis ad vesperam sole fatiscunt vel pluviis,
+          per minutias aurigarum equorumque praecipua ?
+        </p>
 
 
+        <!--2eme image + description-->
+        <img src="../img/pagehorscourse/img2.png" class="">
+
+        <!--Para normal-->
+        <p>
+           aut pugnaciter aleis certant turpi sono fragosis naribus introrsum reducto spiritu concrepantes;
+           aut quod est studiorum omnium maximum ab ortu lucis ad vesperam sole fatiscunt vel pluviis,
+           per minutias aurigarum equorumque praecipua vel delicta scrutantes.aut pugnaciter aleis certant turpi
+           sono fragosis naribus introrsum reducto spiritu concrepantes;
+           aut quod est studiorum omnium maximum ab ortu lucis ad vesperam sole fatiscunt vel pluviis
+        </p>
+
+        <p>
+          Sed ego Atratino, humanissimo atque optimo adulescenti meo necessario, ignosco,
+          qui habet excusationem vel pietatis vel necessitatis vel aetatis.
+          Nec descensurum quemquam ad hanc accusationem fuisse, cui, utrum vellet
+        </p>
+
+        <p>
+          Atratino, humanissimo atque optimo adulescenti meo necessario, ignosco,
+          qui habet excusationem vel pietatis vel necessitatis vel aetatis.
+          Si voluit accusare, pietati tribuo, si iussus est, necessitati, si speravit aliquid, pueritiae.
+          Ceteris non modo nihil ignoscendum, sed etiam acriter est resistendum ?
+        </p>
+
+        <!--Bloque Partager cet article-->
+        <div class="">
+
+          <!--Titre-->
+          <h2 class="">Partager cet article</h2>
+
+          <!--Liens sociaux-->
+
+          <!--fb-->
+          <button type="button" href="#" class="">
+            <img src="../img/pagehorscourse/btfb.png" class="">
+          </button>
+
+          <!--Twitter-->
+          <button type="button" href="#" class="">
+            <img src="../img/pagehorscourse/bttwitter.png" class="">
+          </button>
+
+          <!--Google+-->
+          <button type="button" href="#" class="">
+            <img src="../img/pagehorscourse/btgoogle.png" class="">
+          </button>
+
+        </div>
+
+
+
+      </section>
+
+
+
+
+    </main>
 
     <!--Footer-->
     <?php include('./include/footer.html')?>
