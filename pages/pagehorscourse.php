b8b7b30ebbc39d24256a9f1b15af21ebe9f090f3
Lucien <lucien.crn@gmail.com>
2018-11-29 10:59:27 +0100

    PUSH MASTER

---
+++ pages/pagehorscourse.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Page Hors Courses</title>
+    <link rel="stylesheet" type="text/css" href="../css/pagehorsecourse.css">
+  </head>
+  <body>
+
+    <!--Header-->
+    <?php  include('./include/header.html'); ?>
+
+    <!--Fil d'ariane-->
+
+
+
+    <!--Footer-->
+    <?php include('./include/footer.html')?>
+
+  </body>
+</html>
